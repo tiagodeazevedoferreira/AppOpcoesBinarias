455779cc93515f60467da8dfbf2280a549e54130
tiagodeazevedoferreira <tiago.aferreira@gmail.com>
2026-08-19 14:22:32 -0300

    fix: modernize tick storage imports

--- src/app_opcoes_binarias/data/tick_storage.py
+++ src/app_opcoes_binarias/data/tick_storage.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Any, Iterable
+from collections.abc import Iterable
+from typing import Any
 
 from app_opcoes_binarias.data.firebase_store import FirebaseStore
 
